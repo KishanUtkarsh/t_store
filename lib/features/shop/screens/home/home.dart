926bcfcbc4c94113e1621c6b4c47c6386c5a312d
kishanUtkarsh <kishan.utkarsh12082002@gmail.com>
2025-02-02 19:09:58 +0530

    feat: Added Custom searchbar & ListView Categories.

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:t_store/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:t_store/features/shop/screens/home/widgets/home_categories.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
+import 'package:t_store/utils/constants/sizes.dart';
 
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
+import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
+import '../../../../common/widgets/image_text_widget/vertical_image_text.dart';
+import '../../../../common/widgets/text/section_heading.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,25 +16,53 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
+        body: SingleChildScrollView(
+      child: Column(
+        children: [
+          /// Header Contents
+          TPrimaryHeaderContainer(
+            child: Column(
+              children: [
+                /// Appbar
+                const THomeAppbar(),
+                const SizedBox(
+                  height: TSizes.spaceBtwSections,
+                ),
 
-        child: Column(
-          children: [
-            /// Header Contents
-            TPrimaryHeaderContainer(
-              child: Column(
-                children: [
-                  const THomeAppbar(),
+                /// Searchbar
 
-                ],
-              ),
+                const TSearchContainer(text: 'Search In Store'),
+                const SizedBox(
+                  height: TSizes.spaceBtwSections,
+                ),
+
+                /// Categories
+
+                Padding(
+                  padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                  child: Column(
+                    children: [
+                      /// Heading
+                      const TSectionHeading(
+                        title: 'Popular Categories',
+                        showActionButton: false,
+                        textColor: TColors.white,
+                      ),
+                      const SizedBox(
+                        height: TSizes.spaceBtwItems,
+                      ),
+
+                      /// Categories
+
+                      const THomeCategories()
+                    ],
+                  ),
+                )
+              ],
             ),
-
-          ],
-        ),
-      )
-    );
+          ),
+        ],
+      ),
+    ));
   }
 }
-
-
